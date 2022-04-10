600e610bc345385b84ce57d311d5eafd6b8b255c
Rue Yokaze <yokaze.rue@gmail.com>
2022-04-10 16:45:30 +0900

    update a post

---
+++ template/2018/01/28-1.cpp
@@ -0,0 +1,16 @@
+#include <map>
+
+int main()
+{
+    std::map<int, int> m = {
+        {1, 1},
+        {2, 4},
+        {3, 9},
+        {4, 16},
+    };
+    for (const std::pair<int, int>& kv : m)
+    {
+        printf("%d x %d = %d\n", kv.first, kv.first, kv.second);
+    }
+    return 0;
+}
